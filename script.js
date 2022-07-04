667183d91c274b2382b83f83f2b9f769b6121cdf
CoreyAI <coreyiaquinta@gmail.com>
2022-07-04 19:07:48 -0400

    Added basic tic-tac-toe grid interactivity

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-const gameBoard = (() => {
-  let board = ['c1','c2','c3','c4','c5','c6','c7','c8','c9'];
-
-  // const setBoard = () => {
-  //   const gameContainer = document.querySelectorAll(".game-container");
-
-  // }
-  
-  return {board};
-
-})();
-
-// const Player = () => {
-//   const array = [];
-//   const addTally = (a) => array.push(a);
-//   const resetArray = () => array.slice(0, array.length);
-//   return {array, addTally, resetArray};
-// }
-
 const Player = (name, mark) => {
   const getName = () => name;
   const getMark = () => mark;
   return {name, mark};
 }
 
-const gameController = (() => {
+const gameBoard = (() => {
+  // Initializes game board array values.
+  let getBoard = ['','','','','','','','',''];
 
-})();
-
-const displayController = (() => {
-
-  const setBoard = () => {
-    const gameContainer = document.querySelectorAll(".game-container > div");
-    gameContainer.forEach((cell, index) => {
-      cell.textContent = gameBoard.board[index];
+  // Adds interactivity with tic-tac-toe grid and calls upon the
+  // playRound function.
+  const gameContainer = document.querySelectorAll(".game-container > div");
+  gameContainer.forEach(cell => {
+    cell.addEventListener("click", (e) => {
+      let cellClassName = e.target.className;
+      let cellValue = e.target.innerText;
+      gameController.playRound(cellClassName, cellValue);
     });
-  };
+  });
+  
+  // Inserts player mark into tic-tac-toe cell.
+  const placeMark = (cellName) => {
+    let cell = document.getElementsByClassName(cellName);
+    cell[0].innerText = gameController.currentPlayerMark();
+  }
 
-  return {setBoard};
-
+  return {getBoard, placeMark};
 })();
 
+const gameController = (() => {
+  // Initialize player variables.
+  const player1 = Player("player1", "X");
+  const player2 = Player("player2", "O");
+  let round = 1;
+  
+  // Returns the mark of the current player at a specific round.
+  const currentPlayerMark = () => {
+    if (round % 2 == 0) {
+      return player2.mark;
+    }
+    return player1.mark;
+  }
 
+  // Returns the current round value.
+  const currentRound = () => {
+    return round;
+  }
 
-const player1 = Player();
-const player2 = Player();
-displayController.setBoard();
+  // General game logic used to progress and evaluate game state.
+  const playRound = (cellName, cellValue) => {
+    if (cellValue) {
+      return;
+    }
+
+    gameBoard.placeMark(cellName);
+
+    round++;
+    if (round > 9) {
+      return console.log("game over");
+    }
+  }
+
+  return {playRound, currentPlayerMark, currentRound};
+})();
